017f5f29888efb9406723d3b7d01f335cae68fb5
Ethan Soto <ethan.soto@yahoo.com>
2022-12-26 00:03:36 -0600

    PWA deployment

--- utils/firebase.js
+++ utils/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,8 +14,8 @@ const firebaseConfig = {
   projectId: "gigachad-workout-app",
   storageBucket: "gigachad-workout-app.appspot.com",
   messagingSenderId: "222197614398",
-  appId: "1:222197614398:web:d91efc963ce9cc5b690e6f",
-};
+  appId: "1:222197614398:web:d91efc963ce9cc5b690e6f"
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
