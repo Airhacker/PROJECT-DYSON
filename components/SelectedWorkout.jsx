1bbaaa9aef78fe1242e29c5b72a9d83aa8206cfb
Umair Ahmed <umairahmed.dev@gmail.com>
2022-12-30 01:57:15 -0600

    workout section styling

--- components/SelectedWorkout.jsx
+++ components/SelectedWorkout.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import styles from "../styles/SelectedWorkout.module.css";
+import { FaWeightHanging } from "react-icons/fa";
+import { BsArrowRepeat } from "react-icons/bs";
+import { AiOutlineFieldTime } from "react-icons/ai";
 
 const SelectedWorkout = (props) => {
   return (
@@ -10,14 +13,23 @@ const SelectedWorkout = (props) => {
         {props.workout.exerciseList &&
           props.workout.exerciseList.map((exercise) => {
             return (
-              <div key={exercise + exercise.name}>
+              <div
+                key={exercise + exercise.name}
+                className={styles.exerciseContainer}
+              >
                 <hr />
                 <h3>{exercise.name}</h3>
                 <div className={styles.spanContainer}>
                   <span>Sets</span>
-                  <span>lbs</span>
-                  <span>Reps</span>
-                  <span>Time</span>
+                  <span>
+                    <FaWeightHanging /> Weight
+                  </span>
+                  <span>
+                    <BsArrowRepeat /> Reps
+                  </span>
+                  <span>
+                    <AiOutlineFieldTime /> Time
+                  </span>
                 </div>
                 {exercise.sets.map((set) => (
                   <div key={exercise + set} className={styles.inputContainer}>
