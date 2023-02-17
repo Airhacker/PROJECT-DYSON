9a88ba7edea83fcd042370c5cbb3a8267df9442e
Umair Ahmed <umairahmed.dev@gmail.com>
2023-02-17 02:21:25 -0600

    firebase changes

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getMessaging } from "firebase/messaging";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
